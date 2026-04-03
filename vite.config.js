80f6fe3fac947e44de153e7143c127db5cde98e5
kgael <kgael2567@gmail.com>
2026-04-03 01:01:52 -0600

    Cambiar a purpose 192

--- vite.config.js
+++ vite.config.js
@@ -24,16 +24,10 @@ export default defineConfig({
         start_url: "/",
         scope: "/",
         icons: [
-          { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
+          { src: "/pwa-192.png", sizes: "192x192", type: "image/png", purpose: "any maskable" },
           { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
           { src: "/pwa-128.png", sizes: "128x128", type: "image/png" },
           { src: "/pwa-96.png", sizes: "96x96", type: "image/png" },
-          {
-            src: "/pwa-192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable",
-          },
           {
             src: "/pwa-512.png",
             sizes: "512x512",
