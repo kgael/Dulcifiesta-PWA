6ad4e619211ebaec341018001c30a97b7a07c274
kgael <kgael2567@gmail.com>
2026-04-03 00:30:17 -0600

    Quite elementos en vite

--- vite.config.js
+++ vite.config.js
@@ -27,12 +27,6 @@ export default defineConfig({
           { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
           { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
           {
-            src: "/pwa-192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
             src: "/pwa-512.png",
             sizes: "512x512",
             type: "image/png",
